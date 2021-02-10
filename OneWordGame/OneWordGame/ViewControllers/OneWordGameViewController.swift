c153d76f75e2867d6965c6f10bed1258bd3c3ab2
tittaporn <tittaporn_saelee@icloud.com>
2021-02-10 17:07:12 -0600

    Try to work with timerFunction, but not connect to timerLabel yet.

--- OneWordGame/OneWordGame/ViewControllers/OneWordGameViewController.swift
+++ OneWordGame/OneWordGame/ViewControllers/OneWordGameViewController.swift
@@ -17,6 +17,11 @@ class OneWordGameViewController: UIViewController {
     @IBOutlet weak var directionLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     
+    // MARK: - Properties
+    var mockedTarget = "mock"
+    var countdownTimer: Timer!
+    var totalTime = 60
+    
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +29,30 @@ class OneWordGameViewController: UIViewController {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
+    
+    
+    func startTimer() {
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        timerLabel.text = "\(timeFormatted(totalTime))"
+        if totalTime != 0 {
+            totalTime -= 1
+        } else {
+            endTimer()
+        }
+    }
+    
+    func endTimer() {
+        countdownTimer.invalidate()
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
 }
